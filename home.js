f904f1cd91d43bd35bf7374a471dc9bd540b4865
hyeonsunyoon <hyunsun0806@gmail.com>
2026-08-26 14:33:37 -0500

    add p5js

--- home.js
+++ home.js
@@ -1,1 +1,17 @@
 console.log("hello world!")
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    // background(220);
+
+    // ellipse(mouseX, mouseY, 50, 80);
+    rectMode(CENTER);
+    rect(mouseX, mouseY, 50, 30);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}
